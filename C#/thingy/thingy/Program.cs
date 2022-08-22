23a67aa27222ca381ba9148e453a7438c890f02b
ButterBisket789 <cbbre529@learner.dvusd.org>
2022-08-22 08:49:00 -0700

    monday

--- C#/thingy/thingy/Program.cs
+++ C#/thingy/thingy/Program.cs
@@ -9,28 +9,45 @@ namespace thingy
     internal class Program
     {
         static void Main(string[] args)
-        
-       {
+
+        {
+
             int correct = 0;
-            
-            Console.WriteLine("how many phones are in connors backpack  A.1 b.2 c.0");
-            string anwser = Console.ReadLine();
-            if (anwser == "A")
-            {
-               correct=correct+1;
-            }
-            Console.WriteLine("how old is mr clarke A.51 b. 43 c. 96");
-           anwser = Console.ReadLine();
-            if (anwser == "A")
-                correct = correct + 1;
-            Console.WriteLine("is elije striaght A. false b. true");
-            anwser = Console.ReadLine();
-            if (anwser == "A")
-                correct = correct + 1;
-            Console.WriteLine($"you got {correct} right ");
+
+            ShowQuestion("how many phones are in connors backpack", " a)1", " b)2", " c) 0", " d) 6", "c");
+
+
+            ShowQuestion("how old is mr clarke", " a)51", " b)43", " c) 96", " d) 21", "a");
+
+            ShowQuestion("is elije striaght", "a)true", "b)false" , "c)" , "d)",  "b");
+           
+            ShowQuestion("What is 1+1?", " a)1", " b)2", " c) 0", " d) 6","b");
+
+
 
 
             Console.ReadKey();
+        }
+
+        public static void ShowQuestion(string question,
+                                        string answer1,
+                                        string answer2,
+                                        string answer3,
+                                        string answer4,
+                                        string correct)
+        { 
+            Console.WriteLine(question); 
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+            Console.WriteLine(answer3);
+            Console.WriteLine(answer4); 
+            string answer = Console.ReadLine();
+            if (answer.Equals(correct))
+            {
+                Console.WriteLine("you got it correct");
+            } 
+            
+
         }
     }
 }
